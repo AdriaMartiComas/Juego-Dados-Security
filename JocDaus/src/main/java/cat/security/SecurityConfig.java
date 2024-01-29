d7ee035067adc57a91c299f09b126d95a3b75584
Adrià Martí Comas <adriamarticomas@gmail.com>
2024-01-29 19:15:03 +0100

    Update SecurityConfig.java

--- JocDaus/src/main/java/cat/security/SecurityConfig.java
+++ JocDaus/src/main/java/cat/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package cat.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,7 +21,7 @@ public class SecurityConfig {
 
 	@Autowired
 	public SecurityConfig( JwtAuthEntryPoint authEntryPoint) {
-		
+
 		this.authEntryPoint = authEntryPoint;
 	}
 
@@ -40,9 +39,9 @@ public class SecurityConfig {
 			.antMatchers("/auth/**").permitAll()
 			.anyRequest().authenticated()
 			.and()
-			
+
 			.httpBasic();
-		
+
 
 		http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 		return http.build();
@@ -58,7 +57,7 @@ public class SecurityConfig {
 		return new BCryptPasswordEncoder();
 
 	}
-	
+
 	@Bean
 	public JwtAuthenticationFilter jwtAuthenticationFilter () {
 		return new JwtAuthenticationFilter();
