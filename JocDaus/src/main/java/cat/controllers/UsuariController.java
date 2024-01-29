4a116208e05b2647c182323c7148ea280a5bccbc
Adrià Martí Comas <adriamarticomas@gmail.com>
2024-01-29 18:47:28 +0100

    fixed modified password

--- JocDaus/src/main/java/cat/controllers/UsuariController.java
+++ JocDaus/src/main/java/cat/controllers/UsuariController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import cat.model.domain.Usuari;
@@ -16,6 +17,9 @@ public class UsuariController {
 
 	@Autowired
 	private UsuariService usuariService;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	// Mostrar Usuaris
 	@GetMapping("/mostrarUsuaris")
@@ -54,6 +58,7 @@ public class UsuariController {
 		if (!list.isEmpty()) {
 			for (Usuari usuarif : list) {
 				if (usuari.getPk_UsuariID() == usuarif.getPk_UsuariID()) {
+					usuari.setPassword(passwordEncoder.encode(usuari.getPassword()));
 					usuariService.guardar(usuari);
 
 					return new ResponseEntity<Usuari>(usuari, HttpStatus.OK);
